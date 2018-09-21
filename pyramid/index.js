20fedf12c08a95c8dd823f96645edb6dac776f5b
jylei <jlei91@gmail.com>
2018-09-20 23:30:40 -0700

    solution

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,6 +14,15 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+    const midpoint = Math.floor((2 * n - 1) / 2);
+    for(let row = 0; row < n; row++) {
+      let levels = "";
+      for(let col = 0; col < 2 * n - 1; col ++) {
+        midpoint - row <= col && midpoint + row >= col ? levels += "#" : levels += " ";
+      }
+      console.log(levels);
+    }
+}
 
 module.exports = pyramid;
